e9743152d1721f73009909555dc4d0b67fc659ed
Vadim Kaushan <admin@disasm.info>
2019-06-01 15:38:01 +0300

    Update CI script

--- ci/script.sh
+++ ci/script.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
-
-families=$(cat Cargo.toml | grep -E "^stm32[^ ]+ = \[" | cut -d " " -f1)
-
 set -euxo pipefail
 
-for family in $families; do
-    cargo check --no-default-features --features $family
-done
-
+cargo check --no-default-features --features stm32f103xx
+cargo check --no-default-features --features "stm32l4x2xx pac_ep_hack"
 cargo check --no-default-features --features stm32f103xx --target thumbv7m-none-eabi --examples
